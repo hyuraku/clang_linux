f7185cda703a40ba2e1dcfd9a3168175e25220e5
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-11-30 23:00:22 +0900

    show number of lines

--- cat/cat2.c
+++ cat/cat2.c
@@ -10,14 +10,18 @@ main(int argc, char *argv[])
     FILE *f;
     int c;
 
+    int line = 0;
+
     f = fopen(argv[i], "r");
     if(!f){
       perror(argv[i]);
       exit(1);
     }
     while((c = fgetc(f)) != EOF){
+      if (c == '\n') line++;
       if(putchar(c)<0) exit(1);
     }
+    printf("%d\n", line);
     fclose(f);
   }
   exit(0);
