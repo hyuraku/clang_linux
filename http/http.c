dfd820dfd295a59f43b8ad4a3fb70ea9de7f9225
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-12-07 21:55:41 +0900

    add malloc

--- http/http.c
+++ http/http.c
@@ -13,3 +13,14 @@ log_exit(char *fmt, ...)
   va_end(ap);
   exit(1);
 }
+
+
+static void*
+xmalloc(size_t sz)
+{
+  void *p;
+
+  p = malloc(sz);
+  if (!p) log_exit("failed to allocate memory");
+  return p;
+}
